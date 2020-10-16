170885fd3a233d9031fcdfe80426b7e9a7a8d09d
Artem Dudinskyi <artem.dudynskiy@gmail.com>
2020-10-16 18:22:08 +0300

    apply prettier

--- src/SettingsScreen.tsx
+++ src/SettingsScreen.tsx
@@ -1,28 +1,187 @@
 import React from 'react'
-import {ScrollView, StyleSheet} from 'react-native'
-import {Image, Text} from 'react-native-ui-lib'
+import {ScrollView, StatusBar, StyleSheet, NativeModules} from 'react-native'
+import {LoaderScreen, View, Image, Text, Button, Slider, Switch} from 'react-native-ui-lib'
+import {Settings, SettingsService} from './services/settings'
+import {lightModeTheme, darkModeTheme} from './configs/theme'
 import Assets from './assets'
 
-class SettingsScreen extends React.Component {
+interface Props {}
+
+interface State {
+  isLoading: boolean
+  fontSize: number
+  darkMode: boolean
+}
+
+class SettingsScreen extends React.Component<Props, State> {
+  private settingsService: SettingsService
+
+  private defaultSettings: Settings = {
+    fontSize: 17,
+    darkMode: false,
+  }
+
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      isLoading: false,
+      ...this.defaultSettings,
+    }
+
+    this.settingsService = new SettingsService()
+  }
+
+  onLoadSettingsPress = async () => {
+    this.setState({
+      isLoading: true,
+    })
+
+    try {
+      const settings = (await this.settingsService.fetchSettings()) ?? this.defaultSettings
+
+      this.setState(
+        {
+          ...settings,
+          isLoading: false,
+        },
+        this.onApplySettingsPress,
+      )
+    } catch (error) {
+      console.log('Error', error)
+    }
+  }
+
+  onApplySettingsPress = () => {
+    const {fontSize, darkMode} = this.state
+    NativeModules.SettingsNativeManager.setSettings({font_size: fontSize, dark_mode: darkMode})
+  }
+
+  onFontSizeChange = (fontSize: number) => {
+    this.setState({fontSize})
+  }
+
+  onDarkModeChange = (darkMode: boolean) => {
+    this.setState({darkMode})
+
+    StatusBar.setBarStyle(darkMode ? 'light-content' : 'dark-content')
+  }
+
   render() {
+    const {isLoading, fontSize, darkMode} = this.state
+    const isApplyDisabled = fontSize === 0
+
     return (
-      <ScrollView contentContainerStyle={styles.container}>
-        <Image source={Assets.logo.light} resizeMode="contain" style={styles.logo} />
-        <Text>Hello CocoaHeads</Text>
+      <ScrollView
+        style={darkMode ? styles.darkModeBg : styles.lightModeBg}
+        contentContainerStyle={styles.container}
+      >
+        <View style={styles.sectionCenter}>
+          <Image
+            source={darkMode ? Assets.logo.dark : Assets.logo.light}
+            resizeMode="contain"
+            style={styles.logo}
+          />
+        </View>
+
+        <View style={styles.sectionCenter}>
+          <Text style={[styles.title]} darkMode={darkMode}>
+            {'Settings'}
+          </Text>
+        </View>
+
+        <View style={styles.sectionSpread}>
+          <View style={styles.sectionItem}>
+            <Text darkMode={darkMode}>{'Font Size'}</Text>
+          </View>
+          <View style={styles.sectionItem}>
+            <Slider
+              // @ts-ignore
+              darkMode={darkMode}
+              value={fontSize}
+              minimumValue={0}
+              maximumValue={40}
+              onValueChange={this.onFontSizeChange}
+            />
+          </View>
+        </View>
+
+        <View style={styles.sectionSpread}>
+          <Text darkMode={darkMode}>{'Dark Mode'}</Text>
+          <Switch
+            darkMode={darkMode}
+            disabled={isLoading}
+            value={darkMode}
+            onValueChange={this.onDarkModeChange}
+          />
+        </View>
+
+        {isLoading ? (
+          <View style={[styles.sectionCenter]}>
+            <LoaderScreen />
+          </View>
+        ) : (
+          <View style={styles.sectionSpread}>
+            <Button
+              label={'Load defaults'}
+              onPress={this.onLoadSettingsPress}
+              darkMode={darkMode}
+            />
+
+            <Button
+              label={'Apply'}
+              onPress={this.onApplySettingsPress}
+              disabled={isApplyDisabled}
+              darkMode={darkMode}
+            />
+          </View>
+        )}
+
+        <View style={[styles.demo, styles.sectionCenter]}>
+          <Text darkMode={darkMode} style={{fontSize}}>
+            {'Demo Content'}
+          </Text>
+        </View>
       </ScrollView>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  lightModeBg: {
+    backgroundColor: lightModeTheme.backgroundColor,
+  },
+  darkModeBg: {
+    backgroundColor: darkModeTheme.backgroundColor,
+  },
+  sectionCenter: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+  sectionSpread: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    marginBottom: 30,
+  },
+  sectionItem: {
+    flex: 1,
+  },
+  container: {
+    alignItems: 'stretch',
     justifyContent: 'center',
     padding: 20,
   },
   logo: {
     height: 200,
     width: 200,
+  },
+  title: {
+    fontSize: 30,
+  },
+  demo: {
+    paddingTop: 50,
   },
 })
 
--- src/services/settings.ts
+++ src/services/settings.ts
@@ -1,13 +1,26 @@
-// Settings API
-// method: GET
-// endpoint: https://wix-workshop-server.herokuapp.com/api/v1/settings
-// response example:
-// {
-//    status: 'success' | 'error',
-//    data: {
-//      settings: {
-//        fontSize: number
-//        darkMode: bool
-//      }
-//    }
-// }
+export interface Settings {
+  fontSize: number
+  darkMode: boolean
+}
+
+export interface SettingsResponse {
+  status: 'success' | 'error'
+  data: {
+    settings: Settings
+  }
+}
+
+export class SettingsService {
+  _baseUrl = 'https://wix-workshop-server.herokuapp.com/api/v1'
+
+  _getPath = (endpoint: string) => {
+    return `${this._baseUrl}${endpoint}`
+  }
+
+  fetchSettings = async () => {
+    const path = this._getPath('/settings')
+    const response = await fetch(path)
+    const {data} = (await response.json()) as SettingsResponse
+    return data.settings
+  }
+}
